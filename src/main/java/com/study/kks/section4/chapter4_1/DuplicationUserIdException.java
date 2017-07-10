7ffb22e228b3699645be05639be7dece82eb18d9
hiphopper <syberhiphop@gmail.com>
2017-07-10 23:57:34 +0900

    집 | 2017.07.05 | 토비의 스프링3 226p 3.2 완료

---
+++ src/main/java/com/study/kks/section4/chapter4_1/DuplicationUserIdException.java
@@ -0,0 +1,10 @@
+package com.study.kks.section4.chapter4_1;
+
+/**
+ * Created by home on 2017-07-10.
+ */
+public class DuplicationUserIdException extends RuntimeException {
+    public DuplicationUserIdException(Throwable cause) {
+        super(cause);
+    }
+}
