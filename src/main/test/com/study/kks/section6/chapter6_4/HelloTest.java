fd4d566e8ec9fb0b4c123638080ce7c416cabdfe
hiphopper <syberhiphop@gmail.com>
2017-07-20 00:04:13 +0900

    집 | 2017.07.20 | 토비의 스프링3 472p 6.4 진행중

---
+++ src/main/test/com/study/kks/section6/chapter6_4/HelloTest.java
@@ -0,0 +1,75 @@
+package com.study.kks.section6.chapter6_4;
+
+import org.aopalliance.intercept.MethodInterceptor;
+import org.aopalliance.intercept.MethodInvocation;
+import org.junit.Test;
+import org.springframework.aop.Pointcut;
+import org.springframework.aop.PointcutAdvisor;
+import org.springframework.aop.framework.ProxyFactoryBean;
+import org.springframework.aop.support.DefaultPointcutAdvisor;
+import org.springframework.aop.support.NameMatchMethodPointcut;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+public class HelloTest {
+
+    @Test
+    public void proxyFactoryBean(){
+        ProxyFactoryBean pfBean = new ProxyFactoryBean();
+        pfBean.setTarget(new HelloTarget());
+        pfBean.addAdvice(new UppercaseAdvice());
+
+        Hello hello = (Hello) pfBean.getObject();
+        assertThat(hello.sayHello("Toby"), is("HELLO TOBY"));
+        assertThat(hello.sayHi("Toby"), is("HI TOBY"));
+        assertThat(hello.sayThankYou("Toby"), is("THANK YOU TOBY"));
+    }
+
+    @Test
+    public void pointcutAdvisor(){
+        ProxyFactoryBean pfBean = new ProxyFactoryBean();
+        pfBean.setTarget(new HelloTarget());
+
+        NameMatchMethodPointcut pointcut = new NameMatchMethodPointcut();
+        pointcut.setMappedName("sayH*");
+
+        pfBean.addAdvisor(new DefaultPointcutAdvisor(pointcut, new UppercaseAdvice()));
+
+        Hello hello = (Hello) pfBean.getObject();
+        assertThat(hello.sayHello("Toby"), is("HELLO TOBY"));
+        assertThat(hello.sayHi("Toby"), is("HI TOBY"));
+        assertThat(hello.sayThankYou("Toby"), is("Thank You Toby"));
+    }
+
+    class UppercaseAdvice implements MethodInterceptor{
+        @Override
+        public Object invoke(MethodInvocation invocation) throws Throwable {
+            String ret = (String)invocation.proceed();
+            return ret.toUpperCase();
+        }
+    }
+
+    interface Hello{
+        String sayHello(String name);
+        String sayHi(String name);
+        String sayThankYou(String name);
+    }
+
+    class HelloTarget implements Hello{
+        @Override
+        public String sayHello(String name) {
+            return "Hello " + name;
+        }
+
+        @Override
+        public String sayHi(String name) {
+            return "Hi "+name;
+        }
+
+        @Override
+        public String sayThankYou(String name) {
+            return "Thank You "+name;
+        }
+    }
+}
