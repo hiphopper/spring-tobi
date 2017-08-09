e0d151ec6677a76693fd0a0f265d3812ea3b9556
hiphopper <syberhiphop@gmail.com>
2017-08-10 00:11:25 +0900

    집 | 2017.08.10 | 토비의 스프링3 782p 10.1 진행중

--- src/main/test/com/study/kks/section10/chapter10_1/ApplicationContextTest.java
+++ src/main/test/com/study/kks/section10/chapter10_1/ApplicationContextTest.java
@@ -1,8 +1,19 @@
 package com.study.kks.section10.chapter10_1;
 
 import org.junit.Test;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.beans.factory.config.RuntimeBeanReference;
+import org.springframework.beans.factory.support.PropertiesBeanDefinitionReader;
 import org.springframework.beans.factory.support.RootBeanDefinition;
+import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
 import org.springframework.context.support.StaticApplicationContext;
+import org.springframework.util.ClassUtils;
+import org.springframework.util.StringUtils;
+
+import java.io.File;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.not;
@@ -19,7 +30,7 @@ public class ApplicationContextTest {
         StaticApplicationContext ac = new StaticApplicationContext();
         ac.registerSingleton("hello1", Hello.class);
 
-        RootBeanDefinition definition = new RootBeanDefinition(Hello.class);
+        BeanDefinition definition = new RootBeanDefinition(Hello.class);
         definition.getPropertyValues().addPropertyValue("name", "Spring");
         ac.registerBeanDefinition("hello2", definition);
 
@@ -35,5 +46,66 @@ public class ApplicationContextTest {
 
     @Test
     public void registerBeanWithDependency(){
+        StaticApplicationContext ac = new StaticApplicationContext();
+        ac.registerBeanDefinition("printer", new RootBeanDefinition(StringPrinter.class));
+
+        BeanDefinition helloDef = new RootBeanDefinition(Hello.class);
+        helloDef.getPropertyValues().addPropertyValue("name", "Spring");
+        helloDef.getPropertyValues().addPropertyValue("printer", new RuntimeBeanReference("printer"));
+
+        ac.registerBeanDefinition("hello", helloDef);
+
+        Hello hello = ac.getBean("hello", Hello.class);
+        hello.print();
+
+        assertThat(ac.getBean("printer", Printer.class).toString(), is("Hello Spring"));
+    }
+
+    @Test
+    public void genericApplicationContextWithXml(){
+        String resourcePath = StringUtils.cleanPath(ClassUtils.classPackageAsResourcePath(getClass()));
+        GenericApplicationContext ac = new GenericApplicationContext();
+        XmlBeanDefinitionReader reader = new XmlBeanDefinitionReader(ac);
+        reader.loadBeanDefinitions(resourcePath+ File.separator+"genericApplicationContext.xml");
+        ac.refresh();
+
+        Hello hello = ac.getBean("hello", Hello.class);
+        hello.print();
+
+        assertThat(ac.getBean("printer", Printer.class).toString(), is("Hello Spring"));
+        ac.close();
+    }
+
+    @Test
+    public void genericApplicationContextWithProperties(){
+        String resourcePath = StringUtils.cleanPath(ClassUtils.classPackageAsResourcePath(getClass()));
+        GenericApplicationContext ac = new GenericApplicationContext();
+        PropertiesBeanDefinitionReader reader = new PropertiesBeanDefinitionReader(ac);
+        reader.loadBeanDefinitions(resourcePath+ File.separator+"genericApplicationContext.properties");
+        ac.refresh();
+
+        Hello hello = ac.getBean("hello", Hello.class);
+        hello.print();
+
+        assertThat(ac.getBean("printer", Printer.class).toString(), is("Hello Spring"));
+        ac.close();
+    }
+
+    @Test
+    public void genericXmlApplicationContext(){
+        String resourcePath = StringUtils.cleanPath(ClassUtils.classPackageAsResourcePath(getClass()));
+        ApplicationContext ac = new GenericXmlApplicationContext(resourcePath+ File.separator+"genericApplicationContext.xml");
+
+        Hello hello = ac.getBean("hello", Hello.class);
+        hello.print();
+
+        assertThat(ac.getBean("printer", Printer.class).toString(), is("Hello Spring"));
+    }
+
+    @Test
+    public void test(){
+        String path = ClassUtils.classPackageAsResourcePath(getClass());
+        System.out.println(path);
+        System.out.println(StringUtils.cleanPath("com/study/kks/../../../com/study/kks/section10/chapter10_1"));
     }
 }
